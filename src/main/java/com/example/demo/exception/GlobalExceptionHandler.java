53b7d86f69eb739e56cf895341c55ef07e1dc91f
Jianan.Li <jianan.li@thoughtworks.com>
2020-10-29 17:43:42 +0800

    add validation exception handler

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,22 +2,33 @@ package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.TimeZone;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler({PersonNotExistsException.class})
     public ResponseEntity<ErrorResult> handle(PersonNotExistsException ex) {
-        ErrorResult errorResult = new ErrorResult(
-                ex.getMessage());
+        ErrorResult errorResult = new ErrorResult(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResult);
     }
 
     @ExceptionHandler({TrainerNotEnoughException.class})
     public ResponseEntity<ErrorResult> handle(TrainerNotEnoughException ex) {
-        ErrorResult errorResult = new ErrorResult(
-                ex.getMessage());
+        ErrorResult errorResult = new ErrorResult(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResult> handle(MethodArgumentNotValidException ex) {
+        ErrorResult errorResult = new ErrorResult(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
     }
 }
